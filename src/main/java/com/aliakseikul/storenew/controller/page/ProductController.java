8b278972b7736b528b333ca133b8bc7b113aa733
Aliaksei <kulevetsav@gmail.com>
2024-01-12 16:00:01 +0100

    add ProductController getProductByName() and add http link for all controller

--- src/main/java/com/aliakseikul/storenew/controller/page/ProductController.java
+++ src/main/java/com/aliakseikul/storenew/controller/page/ProductController.java
@@ -17,26 +17,32 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/")
+    @GetMapping("/") //http://localhost:8080/api/product/?id=35026fc0-dbfc-4d52-9c1c-a203929ea63d
     public Product getProductById(@RequestParam String id) {
         return productService.findById(id);
     }
 
-    @GetMapping("/all")
+    @GetMapping("/name") //http://localhost:8080/api/product/name?name=Iphone%208
+    public Product getProductByName(@RequestParam String name) {
+        return productService.findByName(name);
+    }
+
+    @GetMapping("/all") //http://localhost:8080/api/product/all
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    @GetMapping("/allByCategory/")
+    @GetMapping("/allByCategory/") //http://localhost:8080/api/product/allByCategory/?category=ELECTRONICS
     public List<Product> getAllProductsByCategory(@RequestParam String category) {
         return productService.getAllProductsByCategory(category);
     }
 
-    @GetMapping("/allByBrand/")
+    @GetMapping("/allByBrand/") //http://localhost:8080/api/product/allByBrand/?brand=APPLE
     public List<Product> getAllProductsByBrand(@RequestParam String brand) {
         return productService.getAllProductsByBrand(brand);
     }
 
+    //http://localhost:8080/api/product/allByPrice/search?minPrice=1000.00&maxPrice=2000
     @GetMapping("/allByPrice/search")
     public List<Product> searchProductsByPriceRange(
             @RequestParam String minPrice,
