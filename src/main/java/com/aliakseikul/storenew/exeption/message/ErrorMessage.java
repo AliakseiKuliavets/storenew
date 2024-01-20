73402dd2872c13a46b72346231bbaf765f114f3b
Aliaksei <kulevetsav@gmail.com>
2024-01-20 14:27:07 +0100

    create Exceptions and create data verification methods

--- src/main/java/com/aliakseikul/storenew/exeption/message/ErrorMessage.java
+++ src/main/java/com/aliakseikul/storenew/exeption/message/ErrorMessage.java
@@ -2,12 +2,14 @@ package com.aliakseikul.storenew.exeption.message;
 
 public class ErrorMessage {
 
+    private ErrorMessage() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String WRONG_ID_LENGTH = "ID length is wrong";
-    public static final String WRONG_ID = "ID is wrong";
     public static final String PRODUCT_NOT_FOUND = "Product not found";
     public static final String USER_NOT_FOUND = "User not found";
     public static final String ORDER_NUMBER_NOT_FOUND = "Order number not found";
-    public static final String PRODUCT_NAME = "Product name cannot be empty or null";
     public static final String DELIVERY_NOT_FOUND = "Delivery not found";
     public static final String CATEGORY_NOT_FOUND = "Category not found or poorly written";
     public static final String BRAND_NOT_FOUND = "Brand not found or poorly written";
